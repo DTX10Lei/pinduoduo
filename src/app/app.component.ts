0d08c05dcb9b8803608790522c10ef79e18ef381
Zbook-Ubuntu <leidtx10@gmail.com>
2020-06-28 08:28:53 +0800

    after 2-5，before generate any sub components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,88 @@
 import { Component } from '@angular/core';
 
+interface TopMenu {
+  title: String;
+  link?: String;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'pinduoduo';
+  selectedIndex = -1;
+  menus: TopMenu[] = [
+    {
+      title: '热门',
+      link: '#1',
+    },
+    {
+      title: '女装',
+      link: '#2',
+    },
+    {
+      title: '鞋包',
+      link: '#3',
+    },
+    {
+      title: '手机',
+      link: '#1',
+    },
+    {
+      title: '食品',
+      link: '#2',
+    },
+    {
+      title: '母婴',
+      link: '#3',
+    },
+    {
+      title: '百货',
+      link: '#1',
+    },
+    {
+      title: '男装',
+      link: '#2',
+    },
+    {
+      title: '内衣',
+      link: '#3',
+    },
+    {
+      title: '电器',
+      link: '#1',
+    },
+    {
+      title: '家纺',
+      link: '#2',
+    },
+    {
+      title: '水果',
+      link: '#3',
+    },
+    {
+      title: '家具',
+      link: '#3',
+    },
+    {
+      title: '美妆',
+      link: '#1',
+    },
+    {
+      title: '男装',
+      link: '#2',
+    },
+    {
+      title: '运动',
+      link: '#3',
+    },
+    {
+      title: '车品',
+      link: '#1',
+    },
+
+  ];
 }
